2b16ddd893df3f0e4b42c4b4e62917184f0fc0fa
JiroFg <mechcromancer@gmail.com>
2022-12-06 14:20:12 -0600

    tabla de ranking

--- js/script.js
+++ js/script.js
@@ -12,11 +12,24 @@ function tabla(response) {
     document.getElementById("tablaEjemplo").innerHTML = text;
 }
 
+function tablaEquipos(response) {
+    const myObj = JSON.parse(JSON.stringify(response));
+    console.log(myObj)
+    let text = "<tbody style='background-color: white;'>"
+    let ranking = 1
+    for (let x in myObj) {
+        text += "<tr><td>" + ranking + "</td><td>" + myObj[x].nombre + "</td><td>" + myObj[x].score + "</td></tr>";
+        ranking++
+    }
+    text += "</tbody>"    
+    document.getElementById("tablaEjemplo").innerHTML = text;
+}
+
 function consultar(){
     axios.get("https://backvolei-production.up.railway.app/")
         .then(function (response) {
             console.log(response);
-            tabla(response.data)
+            tablaEquipos(response.data)
         })
         .catch(function (error) {
             console.log(error);
